e9ca05a6023a508820f035abb4873f658b9c4667
Manthan-mishra <manthan9481147615@gmail.com>
2024-02-25 00:20:44 +0530

    code refactoring in main file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import { connectDB } from "./db/dbConfig.js";
 import "dotenv/config";
+import express from "express";
 
-connectDB();
+const app = express();
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("ERROR: ", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("error is ", err);
+  });
